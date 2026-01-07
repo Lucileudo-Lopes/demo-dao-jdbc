4fd2314d2514b19a38d3038ff6e42eab2d659605
Lucileudo Paiva <lucileudodepaivalopes@gmail.com>
2026-01-07 18:00:42 -0300

    Creating project and git repository

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,38 @@
 package application;
 
-import java.util.Date;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-import model.entities.Department;
-import model.entities.Seller;
+import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		
-		System.out.println(seller);
-		
-		
+		Connection conn = null;
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+	
+			st = conn.prepareStatement(
+					"DELETE FROM department "
+					+ "WHERE "
+					+ "Id = ?");
+
+			st.setInt(1, 5);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		}
+		catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
 	}
 }
