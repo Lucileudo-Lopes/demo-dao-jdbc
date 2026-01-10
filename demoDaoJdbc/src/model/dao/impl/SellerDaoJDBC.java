fbc0eca2a441cb7d0efd8660ad599b3ee94e9ee7
Lucileudo Paiva <lucileudodepaivalopes@gmail.com>
2026-01-10 16:17:30 -0300

    Implement findById method

--- demoDaoJdbc/src/model/dao/impl/SellerDaoJDBC.java
+++ demoDaoJdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -1,33 +1,78 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 
+import db.DB;
+import model.entities.Department;
 import model.entities.Seller;
 
-public class SellerDaoJDBC implements model.dao.SellerDao{
+public class SellerDaoJDBC implements model.dao.SellerDao {
+
+	private Connection conn;
+
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"select seller.*,department.name as DepName "
+					+"from seller inner join department "
+					+"on seller.DepartmentId = department.Id "
+					+"where seller.id = ? ");
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if(rs.next()) {
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));
+				Seller obj = new Seller();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setDepartment(dep);
+				return obj;
+			}
+			return null;
+		}
+		catch(java.sql.SQLException e) {
+		throw new db.DbException(e.getMessage());
+		
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			
+		}
+		
 	}
 
 	@Override
@@ -35,7 +80,5 @@ public class SellerDaoJDBC implements model.dao.SellerDao{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
 
 }
