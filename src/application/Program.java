580c360fa1a0f622cff6a9065f2fb0e2fdafa08a
Lucileudo Paiva <lucileudodepaivalopes@gmail.com>
2026-01-17 14:39:40 -0300

    Reorganização interna do projeto e ajustes de estrutura

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,38 +1,18 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import db.DB;
-import db.DbIntegrityException;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Connection conn = null;
-		PreparedStatement st = null;
-		try {
-			conn = DB.getConnection();
-	
-			st = conn.prepareStatement(
-					"DELETE FROM department "
-					+ "WHERE "
-					+ "Id = ?");
-
-			st.setInt(1, 5);
-			
-			int rowsAffected = st.executeUpdate();
-			
-			System.out.println("Done! Rows affected: " + rowsAffected);
-		}
-		catch (SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
-		} 
-		finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		SellerDao sellerDao = DaoFactory.creatSellerDao();
+		
+		System.out.println("=== Teste 1: seller findById ===");
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 }
