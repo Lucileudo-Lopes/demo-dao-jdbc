9ab3dfc33ac29f7a4ea7532fff31ab45a8170d7c
Lucileudo Paiva <lucileudodepaivalopes@gmail.com>
2026-01-27 21:35:08 -0300

    feat: implement Department update

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -22,10 +22,18 @@ public class Program2 {
 			System.out.println(d);
 		}
 		
-		System.out.println("\n=== TEST 3: insert =======");
+		System.out.println("\n=== TEST 3: insert ===");
 		Department newDepartment = new Department(null, "Musuc");
 		departmentDao.insert(newDepartment);
-		System.out.println("Inserted! new id: " + newDepartment.getId()); 
+		System.out.println("Inserted! new id: " + newDepartment.getId());
+		
+		System.out.println("\n=== TEST 4: update ===");
+		Department dep2 = departmentDao.findById(1);
+		dep2.setName("Food");
+		departmentDao.update(dep2);
+		System.out.println("Update completed");
+		
+
 	}
 }
 
